7c169cfe5fab13396c2ae401fd108f7a35789ff1
Asher Joy <108951296+Josephfallen@users.noreply.github.com>
2022-12-20 10:53:20 -0500

    Update rcon.sh

--- rcon.sh
+++ rcon.sh
@@ -3,6 +3,5 @@ function slowcat(){ while read; do sleep .15; echo "$REPLY"; done; }
 cat $1 | slowcat | nc {SERVER IP} {SERVER RCON PORT}
 
 {RCON PASSWORD CONVERTED TO MD5 HASH}
-{RCON CMD}
-{RCON CMD}
+{RCON RotateMap}
 Disconnect
